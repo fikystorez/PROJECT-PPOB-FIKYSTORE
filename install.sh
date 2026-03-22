6486cd70f63f14b45cc28602f968f9674cc22c62
Fiky Store <masfikystorez@gmail.com>
2026-03-23 04:30:36 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -55,11 +55,16 @@ while true; do
 
             PORT=3000
 
-            echo "[1/8] Mempersiapkan sistem dan menginstal Node.js (Mohon tunggu)..."
+            echo "[1/8] Mempersiapkan sistem dan menginstal Node.js & Library Browser (Mohon tunggu)..."
             apt update
-            apt install curl -y
+            apt install curl wget gnupg -y
+            
+            # Install Node.js
             curl -fsSL https://deb.nodesource.com/setup_20.x | bash -
             apt install -y nodejs
+            
+            # Install Dependencies Puppeteer (Chrome) untuk VPS
+            apt install -y gconf-service libgbm-dev libasound2 libatk1.0-0 libc6 libcairo2 libcups2 libdbus-1-3 libexpat1 libfontconfig1 libgcc1 libgconf-2-4 libgdk-pixbuf2.0-0 libglib2.0-0 libgtk-3-0 libnspr4 libpango-1.0-0 libpangocairo-1.0-0 libstdc++6 libx11-6 libx11-xcb1 libxcb1 libxcomposite1 libxcursor1 libxdamage1 libxext6 libxfixes3 libxi6 libxrandr2 libxrender1 libxss1 libxtst6 ca-certificates fonts-liberation libappindicator1 libnss3 lsb-release xdg-utils
 
             echo "[2/8] Membuat direktori project: $DIR_NAME..."
             mkdir -p $DIR_NAME
